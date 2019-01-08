98ee5c1e963ab55d6a532eb232e68af49cb08fc3
janainafortunato <37214306+janainafortunato@users.noreply.github.com>
2019-01-08 14:28:58 -0300

    arquivo novo

---
+++ index.php
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+  <title>Web Sites Projetos</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <!-- <link href="web.css" type="texto/css" rel="stylesheet">-->
+  <style>
+  .jumbotron {
+      background-color: #00e6e6;
+      color: #000000;
+      padding: 70px 25px;
+  }
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 80%;
+      margin:0 auto;
+  }
+  .bg-grey {
+  	background-color: white; 
+  }
+    @media screen and (max-width: 768px) {
+    .col-sm-4 {
+      text-align: center;
+      margin: 25px 0;
+    }
+  }
+  .thumbnail {
+    padding: 0 0 15px 0;
+    border: none;
+    border-radius: 0;
+}
+.thumbnail img {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 10px;
+}.panel {
+    border: 1px solid #f4511e; 
+    border-radius:0;
+    transition: box-shadow 0.5s;
+}
+.navbar {
+    margin-bottom: 0;
+    background-color: #1affff;
+    z-index: 9999;
+    border: 0;
+    font-size: 12px !important;
+    line-height: 1.42857143 !important;
+    letter-spacing: 4px;
+    border-radius: 0;
+}
+
+.navbar li a, .navbar .navbar-brand {
+    color: #000000 !important;
+}
+
+.navbar-nav li a:hover, .navbar-nav li.active a {
+    color: #000000 !important;
+    background-color: #ffff !important;
+}
+
+.navbar-default .navbar-toggle {
+    border-color: transparent;
+    color: #fff !important;
+}
+  </style>
+</head>
+<body  id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	<div id="about" class="container-fluid">
+    <div id="services" class="container-fluid">
+    <div id="portfolio" class="container-fluid">
+    <div id="pricing" class="container-fluid">
+    <div id="contact" class="container-fluid">
+	<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#myPage">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">HISTÓRIA</a></li>
+        <li><a href="#services">SERVIÇO</a></li>
+        <li><a href="#portfolio">PORTFOLIO</a></li>
+        <li><a href="#pricing">PREÇO</a></li>
+        <li><a href="#contact">CONTATO</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br><br>
+<br><br>
+<div class="container">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="img/img01.jpg" alt="Seja Online" width="300" height="300">
+        <div class="carousel-caption">
+          <h1>Seja Online</h1>
+          <p><h2>Venha desenvolve seu site com a melhor equipe do mercado.</h2></p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/img02.jpg" alt="Venha fazer seu Blog" width="300" height="300">
+        <div class="carousel-caption">
+          <h1>Venha fazer seu Blog</h1>
+          <p><h2>Blogs com 10% de desconto.</h2></p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/img03.jpg" alt="Faça seu Software" width="300" height="300">
+        <div class="carousel-caption">
+          <h1>Faça seu Software</h1>
+          <p><h2>Crie seu software com a nossa equipe, e gerencie o seu negócio.</h2></p>
+        </div>
+      </div>
+      <div class="item">
+        <img src="img/img04.jpg" alt="jogos com desconto de 15%" width="300" height="300">
+        <div class="carousel-caption">
+          <h1>Jogos com desconto de 15%</h1>
+          <p><h2>Entre em contato e solicite jogos educativos para a sua instituição de ensino.</h2></p>
+        </div>
+      </div>
+    </div>
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+<br><br>
+<div class="container-fluid text-center">
+  <h2>SERVIÇOS</h2>
+  <h4>Desenvolva a sua pagina com a melhor equipe.</h4>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-off"></span>
+      <h3>SITES</h3>
+      <h4>Valor:R$ 1.500,00</h4>
+      <p><h4>Pagina web para pesquisas, vendas e divulgações.</h4></p>
+    </div>
+   <div class="col-sm-4">
+      <span class="glyphicon glyphicon-lock"></span>
+      <h3>SOFTWARE</h3>
+      <h4>Valor: R$ 1.400,00</h4>
+      <p><h4>Programas de gerenciamento.</h4></p>
+    </div>
+    </div>
+    <br><br>
+   <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-leaf"></span>
+      <h3>BLOG</h3>
+      <h4>Valor:R$ 900,00</h4>
+      <p><h4>Portais web para criar tendencias, descurtir temas e se comunicar com o publico em geral.</h4></p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-wrench"></span>
+      <h3>JOGOS</h3>
+      <h4>Valor:R$ 1.000,00</h4>
+      <p><h4>Jogos educativos em 2D.</h4></p>
+    </div>
+  </div>
+</div>
+<div class="container-fluid text-center bg-grey">
+  <h2>PORTFOLIO</h2>
+  <h4>Projetos Desenvolvidos</h4>
+  <div class="row text-center">
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="img/img07.png" alt="BLOG">
+        <p><strong>BLOG</strong></p>
+        <p>Cliente My Diary</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="img/img09.png" alt="JOGOS EDUCATIVOS 2D">
+        <p><strong>JOGOS EDUCATIVOS 2D</strong></p>
+        <p>Cliente Colégio São Paulo da Cruz</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="img/img06.png" alt="San Francisco">
+        <p><strong>SITE</strong></p>
+        <p>Cliente Salão de Beleza Beauty Advisor</p>
+      </div>
+    </div>
+</div>
+<br><br>
+<div class="row text-center">
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="img/img10.png" alt="JOGO">
+        <p><strong>JOGO</strong></p>
+        <p>Cliente Instituição Criança Feliz</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="img/img05.png" alt="SITE">
+        <p><strong>SITE</strong></p>
+        <p>Cliente Joalheria Pewdry Co.</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="img/img08.png" alt="San Francisco">
+        <p><strong>SOFTWARE</strong></p>
+        <p>Cliente Confecções Tecidos</p>
+      </div>
+    </div>
+</div>
+<div class="container-fluid bg-grey">
+  <h2 class="text-center">CONTATO</h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <p>Solicite um Orçamento, em 24 horas você será atendido.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span>Pernambuco, Brasil.</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +55 (81)99799-3278</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +55 (81)98461-6717</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +55 (81)99808-2117</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> websites.projetos@gmail.com </p> 
+    </div>
+    <div class="col-sm-7">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Envie uma proposta de projeto." rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button class="btn btn-default pull-right" type="submit">Envia</button>
+        </div>
+      </div> 
+    </div>
+  </div>
+</div>
+<footer class="container-fluid text-center">
+  <a href="#myPage" title="To Top">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a>
+  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com" title="Visit w3schools">www.w3schools.com</a></p> 
+</footer>
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+   // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+</script>
+
+
+</body>
+</html>
